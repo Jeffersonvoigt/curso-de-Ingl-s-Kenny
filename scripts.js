4edd616093a874393cda97bedf044b5fd62b1240
Jeffersonvoigt <jefferson.voigt@hotmail.com>
2025-10-26 18:47:09 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,548 @@
+function verificarRespostas1() {
+  let total = 0;
+  let corretas = 0;
+
+  total++;
+  let r36 = document.getElementById("q36").value.trim().toUpperCase();
+  let a36 = "ARE";
+  let f36 = document.getElementById("f36");
+  if (r36 === a36) {
+      f36.textContent = "✔️";
+      corretas++;
+  } else {
+      f36.textContent = "❌";
+  }
+
+  total++;
+  let r38 = document.getElementById("q38").value.trim().toUpperCase();
+  let a38 = "IS";
+  let f38 = document.getElementById("f38");
+  if (r38 === a38) {
+      f38.textContent = "✔️";
+      corretas++;
+  } else {
+      f38.textContent = "❌";
+  }
+
+  total++;
+  let r39 = document.getElementById("q39").value.trim().toUpperCase();
+  let a39 = "IS";
+  let f39 = document.getElementById("f39");
+  if (r39 === a39) {
+      f39.textContent = "✔️";
+      corretas++;
+  } else {
+      f39.textContent = "❌";
+  }
+
+  total++;
+  let r40 = document.getElementById("q40").value.trim().toUpperCase();
+  let a40 = "ARE";
+  let f40 = document.getElementById("f40");
+  if (r40 === a40) {
+      f40.textContent = "✔️";
+      corretas++;
+  } else {
+      f40.textContent = "❌";
+  }
+
+  total++;
+  let r41 = document.getElementById("q41").value.trim().toUpperCase();
+  let a41 = "IS";
+  let f41 = document.getElementById("f41");
+  if (r41 === a41) {
+      f41.textContent = "✔️";
+      corretas++;
+  } else {
+      f41.textContent = "❌";
+  }
+
+  total++;
+  let r42 = document.getElementById("q42").value.trim().toUpperCase();
+  let a42 = "IS";
+  let f42 = document.getElementById("f42");
+  if (r42 === a42) {
+      f42.textContent = "✔️";
+      corretas++;
+  } else {
+      f42.textContent = "❌";
+  }
+
+  total++;
+  let r43 = document.getElementById("q43").value.trim().toUpperCase();
+  let a43 = "AM";
+  let f43 = document.getElementById("f43");
+  if (r43 === a43) {
+      f43.textContent = "✔️";
+      corretas++;
+  } else {
+      f43.textContent = "❌";
+  }
+
+  total++;
+  let r44 = document.getElementById("q44").value.trim().toUpperCase();
+  let a44 = "ARE";
+  let f44 = document.getElementById("f44");
+  if (r44 === a44) {
+      f44.textContent = "✔️";
+      corretas++;
+  } else {
+      f44.textContent = "❌";
+  }
+
+  total++;
+  let r45 = document.getElementById("q45").value.trim().toUpperCase();
+  let a45 = "ARE";
+  let f45 = document.getElementById("f45");
+  if (r45 === a45) {
+      f45.textContent = "✔️";
+      corretas++;
+  } else {
+      f45.textContent = "❌";
+  }
+
+  total++;
+  let r46 = document.getElementById("q46").value.trim().toUpperCase();
+  let a46 = "IS";
+  let f46 = document.getElementById("f46");
+  if (r46 === a46) {
+      f46.textContent = "✔️";
+      corretas++;
+  } else {
+      f46.textContent = "❌";
+  }
+
+  total++;
+  let r2751 = document.getElementById("q2751").value.trim().toUpperCase();
+  let a2751 = "ARE";
+  let f2751 = document.getElementById("f2751");
+  if (r2751 === a2751) {
+      f2751.textContent = "✔️";
+      corretas++;
+  } else {
+      f2751.textContent = "❌";
+  }
+
+  total++;
+  let r2752 = document.getElementById("q2752").value.trim().toUpperCase();
+  let a2752 = "IS";
+  let f2752 = document.getElementById("f2752");
+  if (r2752 === a2752) {
+      f2752.textContent = "✔️";
+      corretas++;
+  } else {
+      f2752.textContent = "❌";
+  }
+
+  total++;
+  let r2753 = document.getElementById("q2753").value.trim().toUpperCase();
+  let a2753 = "IS";
+  let f2753 = document.getElementById("f2753");
+  if (r2753 === a2753) {
+      f2753.textContent = "✔️";
+      corretas++;
+  } else {
+      f2753.textContent = "❌";
+  }
+
+  total++;
+  let r2754 = document.getElementById("q2754").value.trim().toUpperCase();
+  let a2754 = "ARE";
+  let f2754 = document.getElementById("f2754");
+  if (r2754 === a2754) {
+      f2754.textContent = "✔️";
+      corretas++;
+  } else {
+      f2754.textContent = "❌";
+  }
+
+  total++;
+  let r2755 = document.getElementById("q2755").value.trim().toUpperCase();
+  let a2755 = "IS";
+  let f2755 = document.getElementById("f2755");
+  if (r2755 === a2755) {
+      f2755.textContent = "✔️";
+      corretas++;
+  } else {
+      f2755.textContent = "❌";
+  }
+
+  total++;
+  let r2756 = document.getElementById("q2756").value.trim().toUpperCase();
+  let a2756 = "ARE";
+  let f2756 = document.getElementById("f2756");
+  if (r2756 === a2756) {
+      f2756.textContent = "✔️";
+      corretas++;
+  } else {
+      f2756.textContent = "❌";
+  }
+
+  total++;
+  let r2757 = document.getElementById("q2757").value.trim().toUpperCase();
+  let a2757 = "ARE";
+  let f2757 = document.getElementById("f2757");
+  if (r2757 === a2757) {
+      f2757.textContent = "✔️";
+      corretas++;
+  } else {
+      f2757.textContent = "❌";
+  }
+
+  total++;
+  let r2758 = document.getElementById("q2758").value.trim().toUpperCase();
+  let a2758 = "ARE";
+  let f2758 = document.getElementById("f2758");
+  if (r2758 === a2758) {
+      f2758.textContent = "✔️";
+      corretas++;
+  } else {
+      f2758.textContent = "❌";
+  }
+
+  total++;
+  let r2759 = document.getElementById("q2759").value.trim().toUpperCase();
+  let a2759 = "IS";
+  let f2759 = document.getElementById("f2759");
+  if (r2759 === a2759) {
+      f2759.textContent = "✔️";
+      corretas++;
+  } else {
+      f2759.textContent = "❌";
+  }
+
+  total++;
+  let r2760 = document.getElementById("q2760").value.trim().toUpperCase();
+  let a2760 = "AM";
+  let f2760 = document.getElementById("f2760");
+  if (r2760 === a2760) {
+      f2760.textContent = "✔️";
+      corretas++;
+  } else {
+      f2760.textContent = "❌";
+  }
+
+  total++;
+  let r2761 = document.getElementById("q2761").value.trim().toUpperCase();
+  let a2761 = "ARE";
+  let f2761 = document.getElementById("f2761");
+  if (r2761 === a2761) {
+      f2761.textContent = "✔️";
+      corretas++;
+  } else {
+      f2761.textContent = "❌";
+  }
+
+  total++;
+  let r2762 = document.getElementById("q2762").value.trim().toUpperCase();
+  let a2762 = "IS";
+  let f2762 = document.getElementById("f2762");
+  if (r2762 === a2762) {
+      f2762.textContent = "✔️";
+      corretas++;
+  } else {
+      f2762.textContent = "❌";
+  }
+
+  total++;
+  let r2763 = document.getElementById("q2763").value.trim().toUpperCase();
+  let a2763 = "IS";
+  let f2763 = document.getElementById("f2763");
+  if (r2763 === a2763) {
+      f2763.textContent = "✔️";
+      corretas++;
+  } else {
+      f2763.textContent = "❌";
+  }
+
+  total++;
+  let r2764 = document.getElementById("q2764").value.trim().toUpperCase();
+  let a2764 = "ARE";
+  let f2764 = document.getElementById("f2764");
+  if (r2764 === a2764) {
+      f2764.textContent = "✔️";
+      corretas++;
+  } else {
+      f2764.textContent = "❌";
+  }
+
+  
+  total++;
+  let r2765 = document.getElementById("q2765").value.trim().toUpperCase();
+  let a2765 = "ARE";
+  let f2765 = document.getElementById("f2765");
+  if (r2765 === a2765) {
+      f2765.textContent = "✔️";
+      corretas++;
+  } else {
+      f2765.textContent = "❌";
+  }
+
+  let nota = (corretas / total) * 10;
+  document.getElementById("nota-final").textContent = "NOTA FINAL: " + nota.toFixed(2);
+
+    alert("Você acertou " + corretas + " de " + total + " questões.");
+}
+
+//------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------
+
+function verificarRespostas2() {
+  let total = 0;
+  let corretas = 0;
+  total++;
+  let r58 = document.getElementById("q58").value.trim().toUpperCase();
+  let a58 = "WORKS";
+  let f58 = document.getElementById("f58");
+  if (r58 === a58) {
+      f58.textContent = "✔️";
+      corretas++;
+  } else {
+      f58.textContent = "❌";
+  }
+  total++;
+  let r60 = document.getElementById("q60").value.trim().toUpperCase();
+  let a60 = "SPEAK";
+  let f60 = document.getElementById("f60");
+  if (r60 === a60) {
+      f60.textContent = "✔️";
+      corretas++;
+  } else {
+      f60.textContent = "❌";
+  }
+  total++;
+  let r61 = document.getElementById("q61").value.trim().toUpperCase();
+  let a61 = "HAS";
+  let f61 = document.getElementById("f61");
+  if (r61 === a61) {
+      f61.textContent = "✔️";
+      corretas++;
+  } else {
+      f61.textContent = "❌";
+  }
+  total++;
+  let r62 = document.getElementById("q62").value.trim().toUpperCase();
+  let a62 = "PLAY";
+  let f62 = document.getElementById("f62");
+  if (r62 === a62) {
+      f62.textContent = "✔️";
+      corretas++;
+  } else {
+      f62.textContent = "❌";
+  }
+  total++;
+  let r63 = document.getElementById("q63").value.trim().toUpperCase();
+  let a63 = "GOES";
+  let f63 = document.getElementById("f63");
+  if (r63 === a63) {
+      f63.textContent = "✔️";
+      corretas++;
+  } else {
+      f63.textContent = "❌";
+  }
+  total++;
+  let r64 = document.getElementById("q64").value.trim().toUpperCase();
+  let a64 = "LIVES";
+  let f64 = document.getElementById("f64");
+  if (r64 === a64) {
+      f64.textContent = "✔️";
+      corretas++;
+  } else {
+      f64.textContent = "❌";
+  }
+  total++;
+  let r65 = document.getElementById("q65").value.trim().toUpperCase();
+  let a65 = "WORK";
+  let f65 = document.getElementById("f65");
+  if (r65 === a65) {
+      f65.textContent = "✔️";
+      corretas++;
+  } else {
+      f65.textContent = "❌";
+  }
+  total++;
+  let r66 = document.getElementById("q66").value.trim().toUpperCase();
+  let a66 = "LIKE";
+  let f66 = document.getElementById("f66");
+  if (r66 === a66) {
+      f66.textContent = "✔️";
+      corretas++;
+  } else {
+      f66.textContent = "❌";
+  }
+  total++;
+  let r67 = document.getElementById("q67").value.trim().toUpperCase();
+  let a67 = "LIVE";
+  let f67 = document.getElementById("f67");
+  if (r67 === a67) {
+      f67.textContent = "✔️";
+      corretas++;
+  } else {
+      f67.textContent = "❌";
+  }
+  total++;
+  let r68 = document.getElementById("q68").value.trim().toUpperCase();
+  let a68 = "STUDIES";
+  let f68 = document.getElementById("f68");
+  if (r68 === a68) {
+      f68.textContent = "✔️";
+      corretas++;
+  } else {
+      f68.textContent = "❌";
+  }
+  total++;
+  let r2781 = document.getElementById("q2781").value.trim().toUpperCase();
+  let a2781 = "HAVE";
+  let f2781 = document.getElementById("f2781");
+  if (r2781 === a2781) {
+      f2781.textContent = "✔️";
+      corretas++;
+  } else {
+      f2781.textContent = "❌";
+  }
+  total++;
+  let r2782 = document.getElementById("q2782").value.trim().toUpperCase();
+  let a2782 = "FIXES";
+  let f2782 = document.getElementById("f2782");
+  if (r2782 === a2782) {
+      f2782.textContent = "✔️";
+      corretas++;
+  } else {
+      f2782.textContent = "❌";
+  }
+  total++;
+  let r2783 = document.getElementById("q2783").value.trim().toUpperCase();
+  let a2783 = "TALKS";
+  let f2783 = document.getElementById("f2783");
+  if (r2783 === a2783) {
+      f2783.textContent = "✔️";
+      corretas++;
+  } else {
+      f2783.textContent = "❌";
+  }
+  total++;
+  let r2784 = document.getElementById("q2784").value.trim().toUpperCase();
+  let a2784 = "WATCHES";
+  let f2784 = document.getElementById("f2784");
+  if (r2784 === a2784) {
+      f2784.textContent = "✔️";
+      corretas++;
+  } else {
+      f2784.textContent = "❌";
+  }
+  total++;
+  let r2785 = document.getElementById("q2785").value.trim().toUpperCase();
+  let a2785 = "LOVE";
+  let f2785 = document.getElementById("f2785");
+  if (r2785 === a2785) {
+      f2785.textContent = "✔️";
+      corretas++;
+  } else {
+      f2785.textContent = "❌";
+  }
+  total++;
+  let r2786 = document.getElementById("q2786").value.trim().toUpperCase();
+  let a2786 = "ENJOYS";
+  let f2786 = document.getElementById("f2786");
+  if (r2786 === a2786) {
+      f2786.textContent = "✔️";
+      corretas++;
+  } else {
+      f2786.textContent = "❌";
+  }
+  total++;
+  let r2787 = document.getElementById("q2787").value.trim().toUpperCase();
+  let a2787 = "SING";
+  let f2787 = document.getElementById("f2787");
+  if (r2787 === a2787) {
+      f2787.textContent = "✔️";
+      corretas++;
+  } else {
+      f2787.textContent = "❌";
+  }
+  total++;
+  let r2788 = document.getElementById("q2788").value.trim().toUpperCase();
+  let a2788 = "DOES";
+  let f2788 = document.getElementById("f2788");
+  if (r2788 === a2788) {
+      f2788.textContent = "✔️";
+      corretas++;
+  } else {
+      f2788.textContent = "❌";
+  }
+  total++;
+  let r2789 = document.getElementById("q2789").value.trim().toUpperCase();
+  let a2789 = "TRIES";
+  let f2789 = document.getElementById("f2789");
+  if (r2789 === a2789) {
+      f2789.textContent = "✔️";
+      corretas++;
+  } else {
+      f2789.textContent = "❌";
+  }
+  total++;
+  let r2790 = document.getElementById("q2790").value.trim().toUpperCase();
+  let a2790 = "LEARN";
+  let f2790 = document.getElementById("f2790");
+  if (r2790 === a2790) {
+      f2790.textContent = "✔️";
+      corretas++;
+  } else {
+      f2790.textContent = "❌";
+  }
+  total++;
+  let r2791 = document.getElementById("q2791").value.trim().toUpperCase();
+  let a2791 = "KNOWS";
+  let f2791 = document.getElementById("f2791");
+  if (r2791 === a2791) {
+      f2791.textContent = "✔️";
+      corretas++;
+  } else {
+      f2791.textContent = "❌";
+  }
+  total++;
+  let r2792 = document.getElementById("q2792").value.trim().toUpperCase();
+  let a2792 = "FLY";
+  let f2792 = document.getElementById("f2792");
+  if (r2792 === a2792) {
+      f2792.textContent = "✔️";
+      corretas++;
+  } else {
+      f2792.textContent = "❌";
+  }
+  total++;
+  let r2793 = document.getElementById("q2793").value.trim().toUpperCase();
+  let a2793 = "TRAVEL";
+  let f2793 = document.getElementById("f2793");
+  if (r2793 === a2793) {
+      f2793.textContent = "✔️";
+      corretas++;
+  } else {
+      f2793.textContent = "❌";
+  }
+  total++;
+  let r2794 = document.getElementById("q2794").value.trim().toUpperCase();
+  let a2794 = "DANCE";
+  let f2794 = document.getElementById("f2794");
+  if (r2794 === a2794) {
+      f2794.textContent = "✔️";
+      corretas++;
+  } else {
+      f2794.textContent = "❌";
+  }
+  total++;
+  let r2795 = document.getElementById("q2795").value.trim().toUpperCase();
+  let a2795 = "WAITS";
+  let f2795 = document.getElementById("f2795");
+  if (r2795 === a2795) {
+      f2795.textContent = "✔️";
+      corretas++;
+  } else {
+      f2795.textContent = "❌";
+  }
+
+  let nota = (corretas / total) * 10;
+  document.getElementById("nota-final").textContent = "NOTA FINAL: " + nota.toFixed(2);
+
+  alert("Você acertou " + corretas + " de " + total + " questões.");
+}
